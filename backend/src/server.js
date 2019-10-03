015edb8aa4caedb57a6a432ca489f4095bbb95b2
Leandro Gabriel <lassisg@gmail.com>
2019-10-03 01:03:51 +0100

    Criação do frontend.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
 const routes = require("./routes");
 
 const app = express();
 
 mongoose.connect(
-  "mongodb+srv://lassisg:lassisg@cluster0-l3knw.mongodb.net/omnistack9?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
+    "mongodb+srv://lassisg:lassisg@cluster0-l3knw.mongodb.net/omnistack9?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
 );
+// GET, POST, PUT, DELETE
 
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+// app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
